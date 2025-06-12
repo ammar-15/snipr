4d7d043b3e314544a6ef06ec9be51ec4072c2230
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2025-06-12 13:08:03 -0230

    FLASK

--- src/lib/app.py
+++ src/lib/app.py
@@ -233,4 +233,4 @@ def analyze():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5274)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
